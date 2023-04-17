806c73b80574c1b263b2d9780813e83045000198
bayoticstudios <abdullahishobaloju@gmail.com>
2023-04-17 22:07:15 +0100

    express fixes for production

--- server/index.js
+++ server/index.js
@@ -20,8 +20,8 @@ import Post from "./models/Post.js";
 import { users, posts } from "./data/index.js";
 
 /* CONFIGURATIONS */
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -53,6 +53,7 @@ app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
+const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '/client/build')));
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/client/build/index.html'))
